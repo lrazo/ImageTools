bac1da07e9ac281699fc19f1fd6354d7a68966e8
lrazo <Ghl1317_>
2022-10-08 17:48:29 +0200

    - Organizing previous code - Adding `remove_background` - Adding `mirror`

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+
 from rembg import remove
 from PIL import Image
 
-if __name__ == '__main__':
-    input_path = 'cl.png'
-    output_path = 'output.png'
+
+def remove_background(input_path='cl.png', output_path='output.png'):
     input = Image.open(input_path)
     output = remove(input)
     output.save(output_path)
 
+
+def mirror(input_path='cl.png', output_path='mirror.png'):
+    img = Image.open(input_path)
+    mirror_image = img.transpose(Image.FLIP_LEFT_RIGHT)
+    mirror_image.save(output_path)
+
+
+if __name__ == '__main__':
+    remove_background()
+    mirror()
